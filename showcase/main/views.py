0bee1a6db146eb47856a913ac1fc22e966f4a174
PezaAntonio <pezaantonio@gmail.com>
2022-10-02 21:35:06 -0700

    Corrected Navbar and side bar. exception caught

--- showcase/main/views.py
+++ showcase/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.exceptions import MultipleObjectsReturned
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import workoutList, workoutEntry
 from .forms import addExercise
@@ -9,7 +10,10 @@ def index(response):
     return render(response, "main/home.html")
 
 def workout(response):
-    w = workoutList.objects.get()
+    try:
+        w = workoutList.objects.get()
+    except MultipleObjectsReturned:
+        w = workoutList.objects.filter().first()
     return render(response, "main/workout.html",{"w":w})
 
 # Form to add a new workout
