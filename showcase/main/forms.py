6f986d9eb1add04622c8fdc07628dc314544b3fc
PezaAntonio <pezaantonio@gmail.com>
2022-10-04 21:04:00 -0700

    More front end changes

--- showcase/main/forms.py
+++ showcase/main/forms.py
@@ -4,6 +4,6 @@ from django import forms
 # inherits from django.forms.Form
 class addExercise(forms.Form):
     exerciseName = forms.CharField(label="Exercise Name", max_length=200, required=True)
-    exerciseSets = forms.CharField(label="Exercise Sets", max_length=4, required=True)
+    exerciseSets = forms.IntegerField(label="Exercise Sets", required=True)
     exerciseReps = forms.IntegerField(label="Exercise Reps", required=True)
     exerciseComment = forms.CharField(label="Exercise Comment", max_length=400, required=True)
