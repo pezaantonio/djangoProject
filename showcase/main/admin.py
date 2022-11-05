3d2fc55285267afbbd5966742de93d450338d688
removedporn <86824510+removedporn@users.noreply.github.com>
2022-11-05 11:39:15 +0800

    Delete showcase directory

--- showcase/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import workoutList
-
-# Register your models here.
-# Registering a model means that it will appear in the admin portal
-admin.site.register(workoutList)
